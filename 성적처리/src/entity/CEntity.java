bc1b93fc79a655b62e775c197c0cee264a5c4977
TaeHoon Kim <overloadkth@naver.com>
2015-04-01 13:54:04 +0900

    2015

--- 성적처리/src/entity/CEntity.java
+++ 성적처리/src/entity/CEntity.java
@@ -3,6 +3,10 @@ package entity;
 import java.io.Serializable;
 
 abstract public class CEntity implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	abstract void read();
 	abstract void write();
 	
--- 성적처리/src/entity/CGangjwa.java
+++ 성적처리/src/entity/CGangjwa.java
@@ -1,5 +1,4 @@
 package entity;
-import java.io.Serializable;
 import java.util.Vector;
 
 
